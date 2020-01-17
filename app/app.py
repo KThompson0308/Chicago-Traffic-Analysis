933788e2f61f316d2ab268a3ab07db2c4d248d98
kevin <kthompson395@hotmail.com>
2020-01-17 17:36:38 -0600

    created jupyter dash app file

---
+++ app/app.py
@@ -0,0 +1,11 @@
+import os
+
+import numpy as np
+import pandas as pd
+import dash
+import dash_core_components as dcc
+import dash_html_components as html
+from dash.dependencies import Input, Output, State, Event
+from jupyter_plotly_dash import JupyterDash
+
+app = JupyterDash('Chicago Car Crashes')
